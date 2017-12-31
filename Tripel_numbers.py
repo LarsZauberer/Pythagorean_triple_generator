35f1e427998a065ae88e9f42bdd481d18e58a892
Nobodysshadow <guenther.wasser@gmail.com>
2017-12-31 18:38:59 +0100

    Fixed the LIST Option

--- Tripel_numbers.py
+++ Tripel_numbers.py
@@ -7,19 +7,19 @@ Returns LIST of pythagorean triple numbers in the given range
 import math
 
 # Define the search function
-def search_numbers(from_nbr, to_nbr, save_type, listing):
+def search_numbers(from_nbr, to_nbr, save_type):
     """search_numbers(from_nbr, to_nbr, save_type, listing).
     -> returns LIST of pythagorean triple numbers in the given range
     Keywords:
     from_nbr:   starting number as base of the triple
     to_nbr:     highest number as base of the triple
     save_type:  SHOW, LIST or SAVE
-    listing:    Listobject to return the LIST if save_type is 'LIST'
     """
     var_a = int(from_nbr)
     var_b = var_a
     var_c = 0
     var_s = 1
+    listing = []
     while var_s:
         # Calculation
         var_c = math.sqrt(var_a*var_a+var_b*var_b)
@@ -28,7 +28,7 @@ def search_numbers(from_nbr, to_nbr, save_type, listing):
             if save_type == "SHOW":
                 print(str(var_a) + "/" + str(var_b) + "/" + str(int(var_c)))
             else:
-                listing.insert(str(var_a) + "/" + str(var_b) + "/" + str(int(var_c)))
+                listing.append(str(var_a) + " / " + str(var_b) + " / " + str(int(var_c)) + " \n")
         # Is the end reached?
         if var_b == int(to_nbr):
             if var_a == int(to_nbr):
@@ -53,15 +53,16 @@ def search_numbers(from_nbr, to_nbr, save_type, listing):
 # Input of range with verification on correct input
 try:
     TEST_FROM = float(input("Number to Start from (FROM): "))
-except:
+except ValueError:
     print("Please type in a number!")
 try:
     TEST_TO = float(input("Maximum Number to go (TO):   "))
-except:
+except ValueError:
     print("Please type in a number!")
 print("OK : From " + str(TEST_FROM) +" to " + str(TEST_TO))
 print("Starting by rounding the numbers")
-search_numbers(int(round(float(TEST_FROM))), int(round(float(TEST_TO))), "SHOW", 0)
-
+ERGEBNIS = search_numbers(int(round(float(TEST_FROM))), int(round(float(TEST_TO))), "LIST")
+if ERGEBNIS != 0:
+    print(ERGEBNIS)
 print("Ende: Please press a ENTER!")
 END_PROGRAM = input()
