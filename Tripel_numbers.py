eb0f6f705abb7a12ff3543d00bbccbd3c16faa20
LarsZauberer <wasser.ian@gmail.com>
2017-12-31 15:23:21 +0100

    function (error)

--- Tripel_numbers.py
+++ Tripel_numbers.py
@@ -1,35 +1,42 @@
-#Imports
-import math
-
-#Variables
-S = 0
+#Define the function
+def Search_Numbers(From, To, SaveType, List):
+    import math
+    A = int(From)
+    B = A
+    S = 0
+    while S == 0:
+        #Calculation
+        C = math.sqrt(A*A+B*B)
+        #Is it a tripel?
+        if float(int(C)) == C:
+            if SaveType == "show":
+                print (str(A) + "/" + str(B) + "/" + str(int(C)))
+            else:
+                List.insert(str(A) + "/" + str(B) + "/" + str(int(C)))
+        #Is the end reached?
+        if B == To:
+            if A == To:
+                S = 1
+        #Make B bigger and reset A
+        if A == To:
+            A = B
+            B = B+1
+        #Make A bigger
+        A = A+1
+    if SaveType == "list":
+        return List
+    elif SaveType == "save":
+        with open("Tripels.txt") as f:
+            f.write(List)
+    else:
+        return 0
 
 #Input of range
-print "From:"
-From = raw_input()
-print "To:"
-To = raw_input()
-From = int(From)
-To = int(To)
+print ("From:")
+Test_From = input()
+print ("To:")
+Test_To = input()
 
-#Update Variables
-A = From
-B = A
+Search_Numbers(Test_From, Test_To, "show", 0)
 
-while S == 0:
-    #Calculation
-    C = math.sqrt(A*A+B*B)
-    #Is it a tripel?
-    if float(int(C)) == C:
-        print str(A) + "/" + str(B) + "/" + str(int(C))
-    #Is the end reached?
-    if B == To:
-        if A == To:
-            S = 1
-    #Make B bigger
-    if A == To:
-        A = B
-        B = B+1
-    #Make A bigger
-    A = A+1
-Break = raw_input()
+Break = input()
