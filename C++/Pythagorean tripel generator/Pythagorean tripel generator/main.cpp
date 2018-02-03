1a9daddc865a6b5eefafbcb25ef2b41a679edaec
LarsZauberer <wasser.ian@gmail.com>
2018-02-03 17:32:08 +0100

    Version 1 new C++ version
    A test version of the new C++ version.

---
+++ C++/Pythagorean tripel generator/Pythagorean tripel generator/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int From;
+	int To;
+	int S = 0;
+	cout << "From:";
+	cin >> From;
+	cout << "To:";
+	cin >> To;
+	int A = From;
+	int B = A;
+	float long C;
+	int Break;
+
+	while (S == 0) {
+		C = sqrt(A*A + B*B);
+		int Cr = C;
+		if (Cr == C) {
+			cout << A << "/" << B << "/" << Cr << endl;
+		}
+		if (A == To) {
+			if (B == To) {
+				S = 1;
+			}
+			A = B;
+			B = B + 1;
+		}
+		A = A + 1;
+	}
+	cin >> Break;
+	return 0;
+}
