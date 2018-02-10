758ab32b07295837d4f57df9f715705d9faf425e
LarsZauberer <wasser.ian@gmail.com>
2018-02-10 20:16:26 +0100

    Make the python program as a python module

--- Tripel_numbers.py
+++ Tripel_numbers.py
@@ -1,35 +1,43 @@
 #Imports
 import math
 
-#Variables
-S = 0
+def tripel_numbers_generator(From, To, R_Type):
+        #Variables
+        S = 0
+        list_output = []
+        #Update Variables
+        A = From
+        B = A
+        while S == 0:
+            #Calculation
+            C = math.sqrt(A*A+B*B)
+            #Is it a tripel?
+            if float(int(C)) == C:
+                if R_Type == "print":
+                    print (str(A) + "/" + str(B) + "/" + str(int(C)))
+                elif R_Type == "list":
+                    list_output.append(str(A) + "/" + str(B) + "/" + str(int(C)))
+            #Is the end reached?
+            if B == To:
+                if A == To:
+                    S = 1
+            #Make B bigger
+            if A == To:
+                A = B
+                B = B+1
+            #Make A bigger
+            A = A+1
+        return list_output
 
-#Input of range
-print ("From:")
-From = input()
-print ("To:")
-To = input()
-From = int(From)
-To = int(To)
+if __name__ == "__main__":
+    #Input of range
+    print ("From:")
+    From = input()
+    print ("To:")
+    To = input()
+    From = int(From)
+    To = int(To)
 
-#Update Variables
-A = From
-B = A
+    tripel_numbers_generator(From, To, "print")
 
-while S == 0:
-    #Calculation
-    C = math.sqrt(A*A+B*B)
-    #Is it a tripel?
-    if float(int(C)) == C:
-        print (str(A) + "/" + str(B) + "/" + str(int(C)))
-    #Is the end reached?
-    if B == To:
-        if A == To:
-            S = 1
-    #Make B bigger
-    if A == To:
-        A = B
-        B = B+1
-    #Make A bigger
-    A = A+1
 Break = input()
